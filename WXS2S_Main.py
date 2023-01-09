0c74de63aad8bf27cdf7f49c3fc003c85cd5b2d4
Lilly <my.email.is.not.public@lol.thri>
2023-01-09 20:42:42 +1300

    License

--- WXS2S_Main.py
+++ WXS2S_Main.py
@@ -1,3 +1,11 @@
+"""
+Copyright © Lilly Chapman 2023
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 3 or later of the License.
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>. 
+"""
+
 # <<< Import libraries  >>>
 
 from pysstv.color import Robot36
